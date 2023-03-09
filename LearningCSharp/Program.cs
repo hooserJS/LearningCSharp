9783648efc5a59fe9f832545a876ff84d07ba34c
unknown <unknown>
2023-03-09 21:37:58 +0200

    initial commit

---
+++ LearningCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
