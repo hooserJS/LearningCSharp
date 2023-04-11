80beee2204a2413cc4e1a08d058c6047ff45ee19
unknown <unknown>
2023-04-11 15:00:37 +0300

    ASPCore HW1

---
+++ ASPCoreWebAPI/Cars.cs
@@ -0,0 +1,18 @@
+﻿namespace ASPCoreWebAPI
+{
+    public class Cars
+    {
+        public string Model { get; set; }
+        public int MaintenancePeriod { get; set; }
+        public string Color { get; set; }
+        public int Price { get; set; }
+
+        public Cars(string model, int maintenancePeriod, string color, int price)
+        {
+            Model = model;
+            MaintenancePeriod = maintenancePeriod;
+            Color = color;
+            Price = price;
+        }
+    }
+}
---
+++ ASPCoreWebAPI/Controllers/CarsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualBasic;
+
+namespace ASPCoreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController : ControllerBase
+    {
+
+
+
+        List<Cars> CarList = new List<Cars> {
+        new Cars("Mercedes", 5, "Blue", 1700),
+        new Cars("BMV", 2, "Black", 2700),
+        new Cars("ISUZU", 9, "White", 900),
+        new Cars("VW", 6, "Silver", 1500)
+    };
+        
+
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ILogger<CarsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetCars")]
+
+        public IEnumerable<Cars> Get()
+        {
+            return CarList;
+        }
+    }
+}
